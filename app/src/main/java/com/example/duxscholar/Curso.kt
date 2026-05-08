ebbd226fce2eef870f47e23f2903c5a3d6a0d096
Breno <brenolemessantos@gmail.com>
2026-05-07 23:26:44 -0300

    Data Classes Curso e Disciplina

---
+++ app/src/main/java/com/example/duxscholar/Curso.kt
@@ -0,0 +1,17 @@
+package com.example.duxscholar
+
+data class Curso(
+    val nome: String,
+    val conteudo: String,
+    val duracao_semestres: Int,
+    val capacidade: Int,
+    val turnos: String
+
+)
+
+
+
+
+
+
+
---
+++ app/src/main/java/com/example/duxscholar/Disciplina.kt
@@ -0,0 +1,7 @@
+package com.example.duxscholar
+
+data class Disciplina(
+    val nome: String,
+    val semestre: Int,
+    val turno: String
+)
