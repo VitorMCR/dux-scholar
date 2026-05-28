0b5e5bf8b565a125cb67e1a6c4e35b96bcc45507
Brevinete Lemes Camargo Peliçari Pinto <0040482411005@americana.fatec.br>
2026-05-28 11:11:22 -0300

    Show carteirinha in CarteirinhaActivity (when available).

--- app/src/main/java/com/example/duxscholar/CarteirinhaActivity.kt
+++ app/src/main/java/com/example/duxscholar/CarteirinhaActivity.kt
@@ -1,12 +1,24 @@
 package com.example.duxscholar
 
 import android.os.Bundle
+import android.util.Base64
+import android.view.View
+import android.widget.ImageButton
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import coil.load
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
 
 class CarteirinhaActivity : AppCompatActivity() {
+    lateinit var imgCarteirinha: ImageView
+    lateinit var txtCarteirinhaMissing: TextView
+    val auth: FirebaseAuth = FirebaseAuth.getInstance()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,6 +28,24 @@ class CarteirinhaActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        imgCarteirinha = findViewById(R.id.imgCarteirinha)
+
+        FirebaseDatabase.getInstance().getReference("alunos").child(auth.currentUser!!.uid).child("carteirinha").get().addOnSuccessListener { snapshot ->
+            if (snapshot.exists()) {
+                if (snapshot.toString().isNotBlank()) {
+                    val carteirinhab64 = Base64.decode(snapshot.value.toString(), Base64.DEFAULT)
+                    imgCarteirinha.load(carteirinhab64)
+                } else {
+                    imgCarteirinha.visibility = View.GONE
+                    txtCarteirinhaMissing.visibility = View.VISIBLE
+                }
+            }
+        }
+
+        findViewById<ImageButton>(R.id.imgbtnVoltarCarteirinha).setOnClickListener {
+            finish()
         }
     }
 }
